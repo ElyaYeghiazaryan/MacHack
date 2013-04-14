e1e776aa26079db2aecfd36c6b9cb1e10dc66005
Elya Yeghiazaryan <oyeghiazaryan@macadamian.com>
2013-04-14 12:22:51 +0400

    Updated javascripts

--- BooksVisualInfoCloud/BookVisualInfoWeb/Scripts/rest.js
+++ BooksVisualInfoCloud/BookVisualInfoWeb/Scripts/rest.js
@@ -1,17 +1,49 @@
-(function() {
+(function () {
 	App.rest = App.rest || {};
 
-	var rest = function(url, data) {
+	var rest = function (url, data) {
 		return $.ajax({
-  			type: "GET",
-  			url: url,
-  			data: data || {},
-  			dataType: "json"
+			type: "GET",
+			url: url,
+			data: data,
+			dataType: "json"
 		})
 	}
 
-	App.rest.fetchBooks = function() {
-		return rest("bubble_data.json");
+
+	App.rest.fetchAllBooks = function () {
+		var url = siteRoot + 'Home/GetAllBooks';
+		return rest(url);
 	}
+
+	App.rest.fetchBooks = function () {
+		var url = siteRoot + 'Home/GetBooks';
+		return rest(url);
+	}
+
+	App.rest.fetchBookData = function () {
+		var url = siteRoot + 'Home/GetBookData';
+		var data = {};
+		//return rest(url, {});
+	}
+
+	App.rest.fetchGenres = function () {
+		var url = siteRoot + 'Home/GetGanres';
+		//return rest(url, {});
+	}
+
+	App.rest.fetchAllFilters = function () {
+		var url = siteRoot + 'Home/GetFilters';
+		//return rest(url, {});
+	}
+
+	App.rest.fetchBook = function () {
+		var url = siteRoot + 'Home/GetBookData';
+		var bookId = 1;
+		var data = { BookId: bookId };
+		//return rest(url, data);
+	}
+
+
 
 })();
